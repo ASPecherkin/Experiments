709bd7a9947c953c8f435824bcd46950392025cb
Artem Pecherkin <as.pecherkin@gmail.com>
2016-02-02 14:59:37 +0300

    Add some mutex and goroutines

--- UsersGitHublanguage.go
+++ UsersGitHublanguage.go
@@ -2,43 +2,59 @@ package main
 
 import (
 	"fmt"
-
 	"sync"
-
 	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+	"os"
 )
 
-type Langs struct {
-	sync.RWMutex
-	m map[string]map[string]int
+// LangValue struct for github.Repository.ListLanguages map
+type LangValue struct {
+	Lang string
+	Value int
 }
 
-func (l *Langs) add(name, login string, client *github.Client, wg *sync.WaitGroup) {
-	defer wg.Done()
-	l.Lock()
-	l.m[name], _, _ = client.Repositories.ListLanguages(login, name)
-	fmt.Println(client.Repositories.ListLanguages(login, name))
-	l.Unlock()
+// RepoLang struct for identify repo with his used languages
+type RepoLang struct {
+	RepName string
+    langs LangValue
+}
+
+// Stats for concurrency accessible slice from goroutines
+type Stats struct {
+	sync.RWMutex
+	Statistic []RepoLang
+}
+
+func (s *Stats) add(name, login string, client *github.Client, wg *sync.WaitGroup, ) {
+    defer s.Unlock()
+    defer wg.Done()
+	langs,_,err := client.Repositories.ListLanguages(login,name)
+	if err != nil {
+		fmt.Println(err)
+	}
+	s.Lock()
+    for k,v := range langs {
+		tlang := LangValue{Lang:k, Value: v}
+		s.Statistic = append(s.Statistic, RepoLang{RepName:name, langs:tlang})
+	}
 }
 
 func main() {
-	client := github.NewClient(nil)
-	result := Langs{}
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken:"some token here"})
+	tc := oauth2.NewClient(oauth2.NoContext,ts)
+	client := github.NewClient(tc)
+	result := Stats{Statistic:make([]RepoLang,0,0)}
 	var wg sync.WaitGroup
-
-	repos, _, err := client.Repositories.List("envek", nil)
+	repos, _, err := client.Repositories.List(os.Args[1], nil)
 	if err != nil {
 		fmt.Println(err)
 	}
 	for _, v := range repos {
 		fmt.Println(*v.Name)
-	}
-	wg.Add(1)
-	go result.add("addressable", "envek", client, &wg)
-	if err != nil {
-		fmt.Println(err)
+		wg.Add(1)
+		go result.add(*v.Name,"envek",client,&wg)
 	}
 	wg.Wait()
 	fmt.Println(result)
-
 }
