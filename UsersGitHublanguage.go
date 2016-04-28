0e157c3af9eee2ec060d35bd2bb6e204232b55be
Artem Pecherkin <as.pecherkin@gmail.com>
2016-04-28 14:12:18 +0300

    temporary UserGitHublanguage programm

--- UsersGitHublanguage.go
+++ UsersGitHublanguage.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"github.com/google/go-github/github"
-	"golang.org/x/oauth2"
+	_ "math"
 	"os"
+	"sync"
+
+	"github.com/google/go-github/github"
+	_ "golang.org/x/oauth2"
 )
 
 // LangValue struct for github.Repository.ListLanguages map
 type LangValue struct {
-	Lang string
+	Lang  string
 	Value int
 }
 
@@ -20,39 +22,40 @@ type Stats struct {
 	Statistic map[string][]LangValue
 }
 
-func (s *Stats) add(name, login string, client *github.Client, wg *sync.WaitGroup, ) {
-    defer s.Unlock()
-    defer wg.Done()
-	langs,_,err := client.Repositories.ListLanguages(login,name)
+func (s *Stats) add(name, login string, client *github.Client, wg *sync.WaitGroup) {
+	defer s.Unlock()
+	defer wg.Done()
+	langs, _, err := client.Repositories.ListLanguages(login, name)
 	if err != nil {
 		fmt.Println(err)
 	}
 	s.Lock()
-    for k,v := range langs {
-	    s.Statistic[name] = append(s.Statistic[name], LangValue{Lang:k, Value: v})
+	for k, v := range langs {
+		s.Statistic[name] = append(s.Statistic[name], LangValue{Lang: k, Value: v})
 	}
 }
 
 func main() {
 	login := os.Args[1]
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken:"..."})
-	tc := oauth2.NewClient(oauth2.NoContext,ts)
-	client := github.NewClient(tc)
-	result := Stats{Statistic:make(map[string][]LangValue)}
+	//ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken:"..."})
+	//tc := oauth2.NewClient(oauth2.NoContext,ts)
+	//client := github.NewClient(tc)
+	client := github.NewClient(nil)
+	result := Stats{Statistic: make(map[string][]LangValue)}
 	var wg sync.WaitGroup
 	repos, _, err := client.Repositories.List(login, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
 	for _, v := range repos {
-	   wg.Add(1)
-	   go result.add(*v.Name,login,client,&wg)
+		wg.Add(1)
+		go result.add(*v.Name, login, client, &wg)
 	}
 	wg.Wait()
-	for k,v := range result.Statistic {
+	for k, v := range result.Statistic {
 		fmt.Println(k)
-		for _,pers := range v {
-			fmt.Printf("%v %v \n", pers.Lang,pers.Value)
+		for _, pers := range v {
+			fmt.Printf("%v %v \n", pers.Lang, pers.Value)
 		}
 		fmt.Println("#####")
 	}
